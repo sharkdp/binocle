b4a4ed1e9ab501467a18f757302014b86225b405
David Peter <mail@david-peter.de>
2021-09-27 09:29:26 +0200

    one counts array

--- src/style.rs
+++ src/style.rs
@@ -4,13 +4,13 @@ pub type Color = [u8; 4];
 
 pub trait Style {
     fn init(&mut self, _view: &View) {}
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color;
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color;
 }
 
 pub struct Colorful;
 
 impl Style for Colorful {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(b) = view.byte_at(view_index) {
             [b, b.overflowing_mul(2).0, b.overflowing_mul(4).0, 255]
         } else {
@@ -21,7 +21,7 @@ impl Style for Colorful {
 pub struct Grayscale;
 
 impl Style for Grayscale {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(b) = view.byte_at(view_index) {
             [b, b, b, 255]
         } else {
@@ -33,7 +33,7 @@ impl Style for Grayscale {
 pub struct Category;
 
 impl Style for Category {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(b) = view.byte_at(view_index) {
             if b == 0x00 {
                 [0, 0, 0, 255]
@@ -76,7 +76,7 @@ impl ColorGradient {
 }
 
 impl Style for ColorGradient {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(b) = view.byte_at(view_index) {
             self.byte_color[b as usize]
         } else {
@@ -88,7 +88,7 @@ impl Style for ColorGradient {
 pub struct RGBA;
 
 impl Style for RGBA {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(int) = view.le_u32_at(view_index) {
             int.to_le_bytes()
         } else {
@@ -100,7 +100,7 @@ impl Style for RGBA {
 pub struct ABGR;
 
 impl Style for ABGR {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(int) = view.le_u32_at(view_index) {
             int.to_be_bytes()
         } else {
@@ -112,7 +112,7 @@ impl Style for ABGR {
 pub struct RGB;
 
 impl Style for RGB {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some([r, g, b]) = view.rgb_at(view_index) {
             [r, g, b, 255]
         } else {
@@ -124,7 +124,7 @@ impl Style for RGB {
 pub struct BGR;
 
 impl Style for BGR {
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some([b, g, r]) = view.rgb_at(view_index) {
             [r, g, b, 255]
         } else {
@@ -135,35 +135,41 @@ impl Style for BGR {
 
 pub struct Entropy {
     window_size: usize,
+    window_size_f64: f64,
+    counts: [i32; 256],
 }
 
 impl Entropy {
     pub fn with_window_size(window_size: usize) -> Entropy {
-        Entropy { window_size }
+        Entropy {
+            window_size,
+            window_size_f64: window_size as f64,
+            counts: [0; 256],
+        }
     }
 }
 
 impl Style for Entropy {
     fn init(&mut self, _: &View) {}
 
-    fn color_at_index(&self, view: &View, view_index: isize) -> Color {
+    fn color_at_index(&mut self, view: &View, view_index: isize) -> Color {
         if let Some(bytes) = view.slice_at(view_index, self.window_size) {
-            let mut counts: [i32; 256] = [0; 256];
+            self.counts.fill(0);
+
             for byte in bytes.iter() {
-                counts[*byte as usize] += 1;
+                self.counts[*byte as usize] += 1;
             }
 
             let mut entropy = 0.0f64;
-            for count in counts {
+            for count in self.counts {
                 if count > 0 {
-                    let p = (count as f64) / (self.window_size as f64);
+                    let p = (count as f64) / self.window_size_f64;
                     entropy -= p * p.log2();
                 }
             }
             entropy *= 1.0f64 / 8.0f64;
 
             let color = colorgrad::magma().at(entropy);
-
             [
                 (color.r * 255.0) as u8,
                 (color.g * 255.0) as u8,
