ab1b4a3cb796b5ee2cfd554a75e6dba4e9756b70
David Peter <mail@david-peter.de>
2021-10-03 18:41:11 +0200

    Proper event handling wrt GUI, closes #19

--- src/event_loop.rs
+++ src/event_loop.rs
@@ -100,171 +100,194 @@ pub fn run(filename: &OsStr) -> Result<()> {
 
         // Handle input events
         if input.update(&event) {
-            if !binocle.settings.gui_wants_keyboard && !binocle.settings.gui_wants_mouse {
+            {
                 let mut settings = &mut binocle.settings;
-
-                // Close events
-                if input.key_pressed(VirtualKeyCode::Escape)
-                    || input.key_pressed(VirtualKeyCode::Q)
-                    || input.quit()
-                {
-                    *control_flow = ControlFlow::Exit;
-                    return;
-                }
-
-                // Fullscreen
-                if input.key_pressed(VirtualKeyCode::F) {
-                    if window.fullscreen().is_none() {
-                        window.set_fullscreen(Some(Fullscreen::Borderless(None)));
-                    } else {
-                        window.set_fullscreen(None)
-                    }
-                }
-
-                // Hex view
-                if input.key_pressed(VirtualKeyCode::H) {
-                    settings.hex_view_visible = !settings.hex_view_visible;
-                }
-
-                if input.key_pressed(VirtualKeyCode::Plus)
-                    || input.key_pressed(VirtualKeyCode::NumpadAdd)
-                {
-                    settings.zoom += 1;
-                } else if input.key_pressed(VirtualKeyCode::Minus)
-                    || input.key_pressed(VirtualKeyCode::NumpadSubtract)
-                {
-                    settings.zoom -= 1;
-                }
-
-                if input.key_pressed(VirtualKeyCode::Left) {
-                    settings.width -= 1;
-                } else if input.key_pressed(VirtualKeyCode::Right) {
-                    settings.width += 1;
-                }
 
                 let offset_factor = if input.held_shift() { 1 } else { 160 };
 
-                if input.key_pressed(VirtualKeyCode::Up) {
-                    settings.offset -= offset_factor * settings.width * settings.stride;
-                } else if input.key_pressed(VirtualKeyCode::Down) {
-                    settings.offset += offset_factor * settings.width * settings.stride;
-                }
+                if !settings.gui_wants_keyboard {
+                    // Close events
+                    if input.key_pressed(VirtualKeyCode::Escape)
+                        || input.key_pressed(VirtualKeyCode::Q)
+                        || input.quit()
+                    {
+                        *control_flow = ControlFlow::Exit;
+                        return;
+                    }
 
-                if input.key_pressed(VirtualKeyCode::N) {
-                    settings.offset -= 1;
-                } else if input.key_pressed(VirtualKeyCode::M) {
-                    settings.offset += 1;
-                }
+                    // Fullscreen
+                    if input.key_pressed(VirtualKeyCode::F) {
+                        if window.fullscreen().is_none() {
+                            window.set_fullscreen(Some(Fullscreen::Borderless(None)));
+                        } else {
+                            window.set_fullscreen(None)
+                        }
+                    }
 
-                if input.key_pressed(VirtualKeyCode::Comma) {
-                    settings.stride -= 1;
-                } else if input.key_pressed(VirtualKeyCode::Period) {
-                    settings.stride += 1;
-                }
+                    // Hex view
+                    if input.key_pressed(VirtualKeyCode::H) {
+                        settings.hex_view_visible = !settings.hex_view_visible;
+                    }
 
-                if input.key_pressed(VirtualKeyCode::PageUp) {
-                    settings.offset -= settings.width * settings.stride * (HEIGHT as isize);
-                } else if input.key_pressed(VirtualKeyCode::PageDown) {
-                    settings.offset += settings.width * settings.stride * (HEIGHT as isize);
-                }
+                    if input.key_pressed(VirtualKeyCode::Plus)
+                        || input.key_pressed(VirtualKeyCode::NumpadAdd)
+                    {
+                        settings.zoom += 1;
+                    } else if input.key_pressed(VirtualKeyCode::Minus)
+                        || input.key_pressed(VirtualKeyCode::NumpadSubtract)
+                    {
+                        settings.zoom -= 1;
+                    }
 
-                if input.key_pressed(VirtualKeyCode::Key1) {
-                    settings.stride = 1;
-                } else if input.key_pressed(VirtualKeyCode::Key2) {
-                    settings.stride = 2;
-                } else if input.key_pressed(VirtualKeyCode::Key3) {
-                    settings.stride = 3;
-                } else if input.key_pressed(VirtualKeyCode::Key4) {
-                    settings.stride = 4;
-                } else if input.key_pressed(VirtualKeyCode::Key5) {
-                    settings.stride = 5;
-                } else if input.key_pressed(VirtualKeyCode::Key6) {
-                    settings.stride = 6;
-                } else if input.key_pressed(VirtualKeyCode::Key7) {
-                    settings.stride = 7;
-                } else if input.key_pressed(VirtualKeyCode::Key8) {
-                    settings.stride = 8;
-                } else if input.key_pressed(VirtualKeyCode::Key9) {
-                    settings.stride = 9;
-                }
+                    if input.key_pressed(VirtualKeyCode::Left) {
+                        settings.width -= 1;
+                    } else if input.key_pressed(VirtualKeyCode::Right) {
+                        settings.width += 1;
+                    }
 
-                if input.key_pressed(VirtualKeyCode::Home) {
-                    settings.offset = 0;
-                    settings.offset_fine = 0;
-                } else if input.key_pressed(VirtualKeyCode::End) {
-                    settings.offset = settings.buffer_length
-                        - settings.width * (HEIGHT as isize) * settings.stride;
-                    settings.offset_fine = 0;
-                }
+                    if input.key_pressed(VirtualKeyCode::Up) {
+                        settings.offset -= offset_factor * settings.width * settings.stride;
+                    } else if input.key_pressed(VirtualKeyCode::Down) {
+                        settings.offset += offset_factor * settings.width * settings.stride;
+                    }
 
-                if input.scroll_diff().abs() > 0.5 {
-                    let scroll = input.scroll_diff() as isize;
-                    if input.held_control() {
-                        settings.zoom += scroll;
-                    } else if input.held_alt() {
-                        settings.width += scroll;
-                    } else {
-                        settings.offset -=
-                            offset_factor * scroll * settings.width * settings.stride;
+                    if input.key_pressed(VirtualKeyCode::N) {
+                        settings.offset -= 1;
+                    } else if input.key_pressed(VirtualKeyCode::M) {
+                        settings.offset += 1;
+                    }
+
+                    if input.key_pressed(VirtualKeyCode::Comma) {
+                        settings.stride -= 1;
+                    } else if input.key_pressed(VirtualKeyCode::Period) {
+                        settings.stride += 1;
+                    }
+
+                    if input.key_pressed(VirtualKeyCode::PageUp) {
+                        settings.offset -= settings.width * settings.stride * (HEIGHT as isize);
+                    } else if input.key_pressed(VirtualKeyCode::PageDown) {
+                        settings.offset += settings.width * settings.stride * (HEIGHT as isize);
+                    }
+
+                    if input.key_pressed(VirtualKeyCode::Key1)
+                        || input.key_pressed(VirtualKeyCode::Numpad1)
+                    {
+                        settings.stride = 1;
+                    } else if input.key_pressed(VirtualKeyCode::Key2)
+                        || input.key_pressed(VirtualKeyCode::Numpad2)
+                    {
+                        settings.stride = 2;
+                    } else if input.key_pressed(VirtualKeyCode::Key3)
+                        || input.key_pressed(VirtualKeyCode::Numpad3)
+                    {
+                        settings.stride = 3;
+                    } else if input.key_pressed(VirtualKeyCode::Key4)
+                        || input.key_pressed(VirtualKeyCode::Numpad4)
+                    {
+                        settings.stride = 4;
+                    } else if input.key_pressed(VirtualKeyCode::Key5)
+                        || input.key_pressed(VirtualKeyCode::Numpad5)
+                    {
+                        settings.stride = 5;
+                    } else if input.key_pressed(VirtualKeyCode::Key6)
+                        || input.key_pressed(VirtualKeyCode::Numpad6)
+                    {
+                        settings.stride = 6;
+                    } else if input.key_pressed(VirtualKeyCode::Key7)
+                        || input.key_pressed(VirtualKeyCode::Numpad7)
+                    {
+                        settings.stride = 7;
+                    } else if input.key_pressed(VirtualKeyCode::Key8)
+                        || input.key_pressed(VirtualKeyCode::Numpad8)
+                    {
+                        settings.stride = 8;
+                    } else if input.key_pressed(VirtualKeyCode::Key9)
+                        || input.key_pressed(VirtualKeyCode::Numpad9)
+                    {
+                        settings.stride = 9;
+                    }
+
+                    if input.key_pressed(VirtualKeyCode::Home) {
+                        settings.offset = 0;
+                        settings.offset_fine = 0;
+                    } else if input.key_pressed(VirtualKeyCode::End) {
+                        settings.offset = settings.buffer_length
+                            - settings.width * (HEIGHT as isize) * settings.stride;
+                        settings.offset_fine = 0;
                     }
                 }
 
-                if let Some((x, y)) = input.mouse() {
-                    if input.mouse_pressed(0) {
-                        if input.held_shift() {
-                            mouse_drag_action = MouseDragAction::ControlOffsetFine {
-                                start_x: x,
-                                start_offset_fine: settings.offset_fine,
-                            };
+                if !settings.gui_wants_mouse {
+                    if input.scroll_diff().abs() > 0.5 {
+                        let scroll = input.scroll_diff() as isize;
+                        if input.held_control() {
+                            settings.zoom += scroll;
+                        } else if input.held_alt() {
+                            settings.width += scroll;
                         } else {
-                            mouse_drag_action = MouseDragAction::ControlOffset {
-                                start_y: y,
-                                start_offset: settings.offset,
+                            settings.offset -=
+                                offset_factor * scroll * settings.width * settings.stride;
+                        }
+                    }
+
+                    if let Some((x, y)) = input.mouse() {
+                        if input.mouse_pressed(0) {
+                            if input.held_shift() {
+                                mouse_drag_action = MouseDragAction::ControlOffsetFine {
+                                    start_x: x,
+                                    start_offset_fine: settings.offset_fine,
+                                };
+                            } else {
+                                mouse_drag_action = MouseDragAction::ControlOffset {
+                                    start_y: y,
+                                    start_offset: settings.offset,
+                                };
+                            }
+                        } else if input.mouse_pressed(1) {
+                            mouse_drag_action = MouseDragAction::ControlWidth {
+                                start_x: x,
+                                start_width: settings.width,
                             };
                         }
-                    } else if input.mouse_pressed(1) {
-                        mouse_drag_action = MouseDragAction::ControlWidth {
-                            start_x: x,
-                            start_width: settings.width,
-                        };
                     }
-                }
 
-                if input.mouse_released(0) || input.mouse_released(1) {
-                    mouse_drag_action = MouseDragAction::Nothing;
-                }
+                    if input.mouse_released(0) || input.mouse_released(1) {
+                        mouse_drag_action = MouseDragAction::Nothing;
+                    }
 
-                if input.mouse_held(0) || input.mouse_held(1) {
-                    if let Some((x, y)) = input.mouse() {
-                        let zoom_factor = settings.zoom_factor();
-                        match mouse_drag_action {
-                            MouseDragAction::ControlOffset {
-                                start_y,
-                                start_offset,
-                            } => {
-                                let delta_y = y - start_y;
-                                let min_offset = start_offset % (settings.width * settings.stride);
-                                settings.offset = min_offset.max(
-                                    start_offset
-                                        - (delta_y as isize) * settings.width * settings.stride,
-                                );
+                    if input.mouse_held(0) || input.mouse_held(1) {
+                        if let Some((x, y)) = input.mouse() {
+                            let zoom_factor = settings.zoom_factor();
+                            match mouse_drag_action {
+                                MouseDragAction::ControlOffset {
+                                    start_y,
+                                    start_offset,
+                                } => {
+                                    let delta_y = y - start_y;
+                                    let min_offset =
+                                        start_offset % (settings.width * settings.stride);
+                                    settings.offset = min_offset.max(
+                                        start_offset
+                                            - (delta_y as isize) * settings.width * settings.stride,
+                                    );
+                                }
+                                MouseDragAction::ControlOffsetFine {
+                                    start_x,
+                                    start_offset_fine,
+                                } => {
+                                    let delta_x = x - start_x;
+                                    settings.offset_fine =
+                                        start_offset_fine - (delta_x as isize) * settings.stride;
+                                }
+                                MouseDragAction::ControlWidth {
+                                    start_x,
+                                    start_width,
+                                } => {
+                                    let delta_x = x - start_x;
+                                    settings.width = start_width + (delta_x as isize) / zoom_factor;
+                                }
+                                MouseDragAction::Nothing => {}
                             }
-                            MouseDragAction::ControlOffsetFine {
-                                start_x,
-                                start_offset_fine,
-                            } => {
-                                let delta_x = x - start_x;
-                                settings.offset_fine =
-                                    start_offset_fine - (delta_x as isize) * settings.stride;
-                            }
-                            MouseDragAction::ControlWidth {
-                                start_x,
-                                start_width,
-                            } => {
-                                let delta_x = x - start_x;
-                                settings.width = start_width + (delta_x as isize) / zoom_factor;
-                            }
-                            MouseDragAction::Nothing => {}
                         }
                     }
                 }
